e30111cc8aa3740d7969aa92990f17d0a5c1a350
Karim Abo Agiza <kemoaboagiza26112000@gmail.com>
2022-12-29 18:15:50 +0200

    adding web socket configuration

---
+++ src/main/java/com/chatapp/config/WebSocketConfiguration.java
@@ -0,0 +1,28 @@
+package com.chatapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+
+    /**
+     * adding endpoint for connection
+     * @param registry
+     */
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/connect").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/content");
+        registry.setApplicationDestinationPrefixes("/chat");
+
+    }
+}
